016a0750f91f0b827deeb1e93f1557dc3cccddef
Nadar <git@nadar.io>
2023-11-23 19:08:13 +0000

    extensions

--- src/wysiwyg.ts
+++ src/wysiwyg.ts
@@ -1,9 +1,16 @@
 import StarterKit from '@tiptap/starter-kit'
+import Youtube from '@tiptap/extension-youtube'
+import Image from '@tiptap/extension-image'
+import Link from '@tiptap/extension-link'
 import { generateHTML } from '@tiptap/html'
 
-function wysiwyg(json: any) {
-    //generateHTML(json, [...[StarterKit], ...extensions])
-    return generateHTML(json, [StarterKit])
+function wysiwyg(json: any, extensions?: any) {
+    try {
+        return generateHTML(json, [... [StarterKit, Youtube, Image, Link], ...extensions])
+    } catch (e) {
+        console.error(e)
+        return ""
+    }
 }
 
 export default wysiwyg;
