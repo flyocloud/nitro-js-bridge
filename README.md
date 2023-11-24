214cc16fb85562448ccac8e22b13560d75216f93
Nadar <git@nadar.io>
2023-11-24 09:07:49 +0000

    fast and basic wysiwyg renderer

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Flyo Nitro Js Bridge
 
 This is can be used in backend and frontend projects eitherway to make the bridge between Flyo Cloud and your application.
+
+## WYSIWYG Custom Render
+
+Since flyo uses ProseMirror/TipTap Json, we have built a custom renderer for you to use in your application in order to handle custom nodes and extend the default ones, for example image:
+
+An example usage for common scenarios:
+
+```js
+const html = wysiwyg(model.content.json, {
+  // image object with custom class
+  image: ({ attrs }) => `<img src="${attrs.src}" alt="${attrs.alt}" title="${attrs.title}" class="my-super-duper-responsive-class" />`,
+  // youtube video with custom width and height in order to make it responsive
+  youtube: ({ attrs }) => `<iframe width="560" height="315" src="${attrs.src}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
+  // a custom flyo based node you like to style:
+  accordion: ({ attrs }) => `<details><summary>${attrs.title}</summary>${attrs.text}</details>`,
+})
+```
+
+By default the most common nodes are handled, but you can override them by passing a function to with the correct node name. See the src/wysiwyg.js file for more details and which nodes are handled by default.
--- src/wysiwyg.ts
+++ src/wysiwyg.ts
@@ -1,16 +1,80 @@
-import StarterKit from '@tiptap/starter-kit'
-import Youtube from '@tiptap/extension-youtube'
-import Image from '@tiptap/extension-image'
-import Link from '@tiptap/extension-link'
-import { generateHTML } from '@tiptap/html'
 
-function wysiwyg(json: any, extensions?: any) {
-    try {
-        return generateHTML(json, [... [StarterKit, Youtube, Image, Link], ...extensions])
-    } catch (e) {
-        console.error(e)
-        return ""
+// Rendering logic for the updated ProseMirror JSON
+function wysiwyg(json: any, nodeRenderers: any) {
+
+  const defaultNodeRenderers = {
+    default: ({ type }: { type: any[] }) => `<div style="border:1px solid red">Node "${type}" is not defined.</div>`,
+    doc: ({ content }: { content: any[] }, renderNode: any) => `${content.map(renderNode).join('')}`,
+    heading: ({ content, attrs }: { content: any[], attrs: { level: number } }, renderNode: any) => `<h${attrs.level}>${content.map(renderNode).join('')}</h${attrs.level}>`,
+    paragraph: ({ content }: { content: any[] }, renderNode: any) => `<p>${content.map(renderNode).join('')}</p>`,
+    bulletList: ({ content }: { content: any[] }, renderNode: any) => `<ul>${content.map(child => `<li>${renderNode(child)}</li>`).join('')}</ul>`,
+    orderedList: ({ content }: { content: any[] }, renderNode: any) => `<ol>${content.map(child => `<li>${renderNode(child)}</li>`).join('')}</ol>`,
+    listItem: ({ content }: { content: any[] }, renderNode: any) => `${content.map(child => renderNode(child)).join('')}`,
+    blockquote: ({ content }: { content: any[] }, renderNode: any) => `<blockquote>${content.map(child => renderNode(child)).join('')}</blockquote>`,
+    image: ({ attrs }: { attrs: { src: string, alt: string, title: string } }) => `<img src="${attrs.src}" alt="${attrs.alt}" title="${attrs.title}" />`,
+    youtube: ({ attrs }: { attrs: { src: string, start: string } }) => `<iframe width="560" height="315" src="${attrs.src}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`,
+    text: ({ text, marks }: { text: string, marks: any[] }) => {
+      let renderedText = text;
+      if (marks) {
+        marks.forEach(mark => {
+          if (mark.type === "bold") {
+            renderedText = `<strong>${renderedText}</strong>`;
+          } else if (mark.type === "italic") {
+            renderedText = `<em>${renderedText}</em>`;
+          } else if (mark.type === "underline") {
+            renderedText = `<u>${renderedText}</u>`;
+          } else if (mark.type === "strikethrough") {
+            renderedText = `<del>${renderedText}</del>`;
+          } else if (mark.type === "link") {
+            renderedText = `<a href="${mark.attrs.href}" target="${mark.attrs.target}">${renderedText}</a>`;
+          }
+          // Add more conditions for other mark types (underline, strikethrough, etc.) if needed
+        });
+      }
+      return renderedText;
+    },
+  };
+
+  const combinedNodeRenderers = { ...defaultNodeRenderers, ...nodeRenderers };
+
+  function renderNode(node: any) {
+    const renderer = combinedNodeRenderers[node.type] || combinedNodeRenderers.default;
+    if (renderer) {
+      if (!node.hasOwnProperty('content')) {
+        // this ensures that the content property is always an array which can be a problem with empty paragraphs
+        node.content = []
+      }
+      return renderer(node, renderNode);
     }
+    return ''; // Return an empty string if no renderer is found for the node type
+  }
+
+  return renderNode(json);
 }
+
+// Define rendering callbacks for each node type
+/*
+const nodeRenderers = {
+  default: ({ content }) => `<div style="border:1px solid red">${content.map(renderNode).join('')}</div>`,
+  doc: ({ content }) => `<div>${content.map(renderNode).join('')}</div>`,
+  heading: ({ content, attrs }) => `<h${attrs.level}>${content.map(renderNode).join('')}</h${attrs.level}>`,
+  paragraph: ({ content }) => `<p>${content.map(renderNode).join('')}</p>`,
+  text: ({ text, marks }) => {
+    let renderedText = text;
+    if (marks) {
+      marks.forEach(mark => {
+        if (mark.type === "bold") {
+          renderedText = `<strong>${renderedText}</strong>`;
+        } else if (mark.type === "italic") {
+          renderedText = `<em>${renderedText}</em>`;
+        }
+        // Add more conditions for other mark types (underline, strikethrough, etc.) if needed
+      });
+    }
+    return renderedText;
+  }
+  // Add more node types and their rendering callbacks as needed
+};
+*/
 
 export default wysiwyg;
